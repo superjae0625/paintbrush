0360be2dff2f29d9c2148fe8b8ffc8b6be89f25f
jaehyungpark <superjae0625@gmail.com>
2021-08-31 23:01:48 -0600

    Deployment

--- app.js
+++ app.js
@@ -1,125 +1,109 @@
-html,
-    body,
-    div,
-    span,
-    applet,
-    object,
-    iframe,
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6,
-    p,
-    blockquote,
-    pre,
-    a,
-    abbr,
-    acronym,
-    address,
-    big,
-    cite,
-    code,
-    del,
-    dfn,
-    em,
-    img,
-    ins,
-    kbd,
-    q,
-    s,
-    samp,
-    small,
-    strike,
-    strong,
-    sub,
-    sup,
-    tt,
-var,
-    b,
-    u,
-    i,
-    center,
-    dl,
-    dt,
-    dd,
-    ol,
-    ul,
-    li,
-    fieldset,
-    form,
-    label,
-    legend,
-    table,
-    caption,
-    tbody,
-    tfoot,
-    thead,
-    tr,
-    th,
-    td,
-    article,
-    aside,
-    canvas,
-    details,
-    embed,
-    figure,
-    figcaption,
-    footer,
-    header,
-    hgroup,
-    menu,
-    nav,
-    output,
-    ruby,
-    section,
-    summary,
-    time,
-    mark,
-    audio,
-    video {
-        margin: 0;
-padding: 0;
-border: 0;
-font - size: 100 %;
-font: inherit;
-vertical - align: baseline;
+const canvas = document.getElementById( "jsCanvas" );
+const ctx = canvas.getContext( "2d" );
+const colors = document.getElementsByClassName( "jsColor" );
+const range = document.getElementById( "jsRange" );
+const mode = document.getElementById( "jsMode" );
+const saveBtn = document.getElementById( "jsSave" );
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect( 0, 0, CANVAS_SIZE, CANVAS_SIZE );
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
+
+let painting = false;
+let filling = false;
+
+function stopPainting () {
+    painting = false;
 }
 
-/* HTML5 display-role reset for older browsers */
-article,
-    aside,
-    details,
-    figcaption,
-    figure,
-    footer,
-    header,
-    hgroup,
-    menu,
-    nav,
-    section {
-    display: block;
+function startPainting () {
+    painting = true;
 }
-body {
-    line - height: 1;
+
+function onMouseMove ( event ) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if ( !painting ) {
+        ctx.beginPath();
+        ctx.moveTo( x, y );
+    } else {
+        //connects the last point in the current sub-path to the specified(x, y) coordinates with a straight line
+        ctx.lineTo( x, y );
+        ctx.stroke();
+    }
 }
-ol,
-    ul {
-    list - style: none;
+
+function handleColorClick ( event ) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
-blockquote,
-    q {
-    quotes: none;
+
+function handleRangeChange ( event ) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
 }
-blockquote: before,
-    blockquote: after,
-        q: before,
-            q: after {
-    content: "";
-    content: none;
+
+function handleModeClick () {
+    if ( filling === true ) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
 }
-table {
-    border - collapse: collapse;
-    border - spacing: 0;
+
+function handleCanvasClick () {
+    if ( filling ) {
+        ctx.fillRect( 0, 0, CANVAS_SIZE, CANVAS_SIZE );
+    }
+}
+
+function handleCM ( event ) {
+    event.preventDefault();
+}
+
+function handleSaveClick () {
+    const image = canvas.toDataURL();
+    const link = document.createElement( "a" );
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+}
+
+if ( canvas ) {
+    canvas.addEventListener( "mousemove", onMouseMove );
+    //when clicked
+    canvas.addEventListener( "mousedown", startPainting );
+    //when mouse clicked off
+    canvas.addEventListener( "mouseup", stopPainting );
+    //leave to outside of canvas
+    canvas.addEventListener( "mouseleave", stopPainting );
+    canvas.addEventListener( "click", handleCanvasClick );
+    canvas.addEventListener( "contextmenu", handleCM );
+}
+
+Array.from( colors ).forEach( color =>
+    color.addEventListener( "click", handleColorClick )
+);
+
+if ( range ) {
+    range.addEventListener( "input", handleRangeChange );
+}
+
+if ( mode ) {
+    mode.addEventListener( "click", handleModeClick );
+}
+
+if ( saveBtn ) {
+    saveBtn.addEventListener( "click", handleSaveClick );
 }
